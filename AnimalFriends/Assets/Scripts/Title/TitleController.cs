b4a75e5208363a93bdbee4774144fe1b43e56453
jalemy <jalemy.xps@gmail.com>
2017-06-05 22:59:43 +0900

    Title Scene追加

---
+++ AnimalFriends/Assets/Scripts/Title/TitleController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+	void Update ()
+    {
+        Debug.Log("TEST");
+		if (Input.anyKey)
+		{
+			SceneManager.LoadScene("MainScene");
+		}
+	}
+}
