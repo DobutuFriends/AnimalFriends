b8f155a59ff002f5970b6cd130ec121b4b0d3c18
jalemy <jalemy.xps@gmail.com>
2017-06-06 02:18:38 +0900

    弾を右方向にだけ飛ばせるようにした

---
+++ AnimalFriends/Assets/Scripts/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UniRx;
+using System;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        Observable.Timer(TimeSpan.FromSeconds(5.0f))
+                  .Subscribe(_ => Destroy(gameObject))
+                  .AddTo(this);
+    }
+
+    void Update()
+    {
+        Vector3 vector = new Vector3(speed, 0, 0);
+
+        transform.position += vector;
+    }
+}
