aa678f534d3789c3c86e13ac0e2f49709fa7bc4b
IwakuraS <c011206342@edu.teu.ac.jp>
2017-06-21 22:48:19 +0900

    このブランチはテスト兼コメント残し用

--- AnimalFriends/Assets/Scripts/PlayerControl.cs
+++ AnimalFriends/Assets/Scripts/PlayerControl.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using UniRx;
 using System;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
     public float SPEED;
     public float JUMP_POWER;
     private int isRunningId = Animator.StringToHash("isRunning");
+    private int key;
     private Rigidbody2D rb;
+    private Animator animator;
+    private float walkForce = 60.0f;//歩き力
+    private float maxWalkSpeed = 10.0f;//速度制限
 
     [SerializeField]
     BulletController bullet;
@@ -16,6 +21,7 @@ public class PlayerControl : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         Observable.EveryUpdate()
                   .Where(_ => Input.GetKey("z"))
@@ -27,7 +33,8 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        MoveAddForce();
+        Retry();
     }
 
     private void InstantiateBullet()
@@ -50,12 +57,87 @@ public class PlayerControl : MonoBehaviour
             scale.x = 1;
         }
 
-        if (Input.GetKey("up"))
+        if (Input.GetKey("up") && rb.velocity.y == 0)
         {
             rb.velocity = new Vector2(0, JUMP_POWER);
         }
 
         transform.position = position;
         transform.localScale = scale;
+
+        // プレイヤの速度に応じてアニメーション速度を変える
+        //地面＝歩行アニメ＝ジャンプ中ではない
+        if (rb.velocity.y == 0)
+        {
+            this.animator.speed = SPEED / 2.0f;
+
+        }
+    }
+
+
+    void MoveAddForce()
+    {
+        // 左右移動
+        key = 0;//何も押されていないときは0とすること＝歩き力0
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // プレイヤの速度
+        SPEED = Mathf.Abs(rb.velocity.x);
+
+        // スピード制限：MAXスピード未満なら加速（減速）させる
+        if (SPEED < this.maxWalkSpeed)
+        {
+            rb.AddForce(transform.right * key * this.walkForce);
+        }
+
+
+        // 反転対策：歩く向きによって、体を反転させる
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+
+        // ジャンプする
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
+        {
+            rb.AddForce(transform.up * JUMP_POWER);
+        }
+
+
+        // プレイヤの速度に応じてアニメーション速度を変える
+        //地面＝歩行アニメ＝ジャンプ中ではない
+        if (rb.velocity.y == 0)
+        {
+            this.animator.speed = SPEED / 2.0f;
+
+        }
+        //ジャンプ中のアニメーション速度を一定とする
+        else
+        {
+            this.animator.speed = 1.5f;
+        }
+
+    }
+
+
+
+
+    void Retry()
+    {
+            if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+
+
+    // ゴールに到達
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ゴール");
+        //SceneManager.LoadScene("ClearScene");
     }
 }
