78bc7f65d03435bb4972b6f31324f89bd64e18b6
IwakuraS <c011206342@edu.teu.ac.jp>
2017-06-23 13:55:01 +0900

    HPgauge追加

--- AnimalFriends/Assets/Scripts/CameraControl.cs
+++ AnimalFriends/Assets/Scripts/CameraControl.cs
@@ -1,24 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
+public class CameraControl : MonoBehaviour
+{
 
-	private GameObject player = null;
-	private Vector3 offset = Vector3.zero;
+    private GameObject player = null;
+    private Vector3 offset = Vector3.zero;
 
-	void Start()
-	{
-		player = GameObject.FindGameObjectWithTag("Player");
-		offset = transform.position - player.transform.position;
-	}
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position - player.transform.position;
+    }
 
-	void LateUpdate()
-	{
-		Vector3 newPosition = transform.position;
-		newPosition.x = player.transform.position.x + offset.x;
-		newPosition.y = player.transform.position.y + offset.y;
-		newPosition.z = player.transform.position.z + offset.z;
-		transform.position = newPosition;
-	}
+    void LateUpdate()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x = player.transform.position.x + offset.x;
+        newPosition.y = player.transform.position.y + offset.y;
+        newPosition.z = player.transform.position.z + offset.z;
+        transform.position = newPosition;
+    }
 }
